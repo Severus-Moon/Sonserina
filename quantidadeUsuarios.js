ea446486d4bbb566fd4feff9b6ff85ce56102bac
Tinão <106550470+Severus-Moon@users.noreply.github.com>
2024-11-05 10:51:18 -0300

    Add files via upload

---
+++ quantidadeUsuarios.js
@@ -0,0 +1,28 @@
+import { getCSS } from "./common.js"
+
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS 
+            }
+        }   
+    ]
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuariosPorRede()
