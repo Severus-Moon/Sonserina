56f3514a404970b7607e8c9031ab06fb53da39f5
Tinão <106550470+Severus-Moon@users.noreply.github.com>
2024-11-08 09:40:02 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -3,4 +3,11 @@ const getCSS = (variavel) => {
 
 }
 
-export {getCSS}
+const tickConfig = {
+    color: getCSS ('--primary-color'),
+                size: 16,
+                family: getCSS ('--font')
+}
+
+
+export {getCSS, tickConfig}
--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
 
 
 async function quantidadeUsuariosPorRede() {
@@ -29,7 +29,27 @@ async function quantidadeUsuariosPorRede() {
                 color: getCSS ('--primary-color'),
                 family: getCSS ('--font'),
             }
-        }
+        },
+        xaxis: {  
+        
+            tickfont: {
+                color: getCSS ('--primary-color'),
+                size: 16,
+                family: getCSS ('--font')
+
+            },             
+            title: {
+                    text: 'nome das redes sociais',
+                    font: {
+                        color: getCSS ('--secondary-color')
+                    }
+                 }
+          },
+          yaxis: {
+                 title: {
+                    text: 'bilhões de usuários ativos',
+                 }
+          }
     }
 
     const grafico = document.createElement('div')
